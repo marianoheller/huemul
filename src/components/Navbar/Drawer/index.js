b4e6dac30c016ea18bca056845dbb9af9a2ab187
Mariano Mariano <marianofheller@gmail.com>
2018-06-20 19:45:39 -0300

    Added tooltip to menu

--- src/components/Navbar/Drawer/index.js
+++ src/components/Navbar/Drawer/index.js
@@ -5,6 +5,7 @@ import MenuIcon from '@material-ui/icons/Menu';
 import List from '@material-ui/core/List';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import Tooltip from '@material-ui/core/Tooltip';
 import { ArrowBack, Event, Dvr, PermIdentity, Business, Work, Group } from '@material-ui/icons';
 
 import * as SC from './StyledComponents';
@@ -19,57 +20,93 @@ const icons = {
   clientes: <Business />,
 };
 
-export default function Drawer(props) {
-  const {
-    isOpen,
-    onClose,
-    onClick,
-    name,
-    items,
-  } = props;
+export default class Drawer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      tooltipOpen: false,
+    };
+    this.handleTooltipClose = this.handleTooltipClose.bind(this);
+    this.handleTooltipOpen = this.handleTooltipOpen.bind(this);
+  }
 
-  return (
-    <SC.LeftMenuContainer>
-      <IconButton
-        aria-owns={isOpen ? `drawer-menu-${name}` : null}
-        aria-haspopup="true"
-        onClick={!isOpen ? onClick : onClose}
-        color="inherit"
-      >
-        { !isOpen ?
-          <MenuIcon color="secondary" />
-          :
-          <ArrowBack color="secondary" />
-        }
-      </IconButton>
-      <CustomDrawer
-        onClose={onClose}
-        isOpen={isOpen}
-      >
-        <List>
-          {items.map((item, i) => {
-            switch (item.type) {
-              case 'item':
-              return (
-                <SC.StyledNavLink to={item.route} key={`${item.route}${i}`}>
-                  <SC.StyledListItem button>
-                    <ListItemIcon>
-                      {icons[item.icon] || <MenuIcon /> }
-                    </ListItemIcon>
-                    <ListItemText primary={item.text} />
-                  </SC.StyledListItem>
-                </SC.StyledNavLink>
-              );
-              case 'divider':
-              return <SC.StyledDivider key={`${item.type}${i}`} />;
-              default:
-              return null;
+  componentDidMount() {
+    setTimeout(this.handleTooltipOpen, 1500);
+  }
+
+  handleTooltipClose() {
+    this.setState({ tooltipOpen: false });
+  }
+
+  handleTooltipOpen(e) {
+    // Filter out focus event (it triggers automatically for some reason)
+    if (e && e.type === 'focus') return;
+    this.setState({ tooltipOpen: true });
+  }
+
+  render() {
+    const {
+      isOpen,
+      onClose,
+      onClick,
+      name,
+      items,
+    } = this.props;
+
+    const { tooltipOpen } = this.state;
+    return (
+      <SC.LeftMenuContainer>
+        <Tooltip
+          enterDelay={500}
+          onClose={this.handleTooltipClose}
+          onOpen={this.handleTooltipOpen}
+          open={tooltipOpen}
+          placement="bottom"
+          title="Click here to navigate"
+        >
+          <IconButton
+            aria-owns={isOpen ? `drawer-menu-${name}` : null}
+            aria-haspopup="true"
+            onClick={!isOpen ? onClick : onClose}
+            color="inherit"
+          >
+            { !isOpen ?
+              <MenuIcon color="secondary" />
+              :
+              <ArrowBack color="secondary" />
             }
-          })}
-        </List>
-      </CustomDrawer>
-    </SC.LeftMenuContainer>
-  );
+          </IconButton>
+        </Tooltip>
+
+        <CustomDrawer
+          onClose={onClose}
+          isOpen={isOpen}
+        >
+          <List>
+            {items.map((item, i) => {
+              switch (item.type) {
+                case 'item':
+                return (
+                  <SC.StyledNavLink to={item.route} key={`${item.route}${i}`}>
+                    <SC.StyledListItem button>
+                      <ListItemIcon>
+                        {icons[item.icon] || <MenuIcon /> }
+                      </ListItemIcon>
+                      <ListItemText primary={item.text} />
+                    </SC.StyledListItem>
+                  </SC.StyledNavLink>
+                );
+                case 'divider':
+                return <SC.StyledDivider key={`${item.type}${i}`} />;
+                default:
+                return null;
+              }
+            })}
+          </List>
+        </CustomDrawer>
+      </SC.LeftMenuContainer>
+    );
+  }
 }
 
 Drawer.propTypes = {
