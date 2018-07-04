b48d3d798d29064ed428723751b52bc9aeae81e1
Mariano Heller <marianofheller@gmail.com>
2018-07-04 12:33:50 -0300

    config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import axiosRef from 'axios';
 
 export const axios = axiosRef.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://some-domain.com/' : 'http://localhost:3001',
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://rugged-virgin-islands-18071.herokuapp.com' : 'http://localhost:3001',
   timeout: 1000,
 });
 
