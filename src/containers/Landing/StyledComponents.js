496373466ad69d05b68a4deb452f8a95f96cead8
Mariano Heller <marianofheller@gmail.com>
2018-07-11 12:32:35 -0300

    pwa

--- src/containers/Landing/StyledComponents.js
+++ src/containers/Landing/StyledComponents.js
@@ -2,26 +2,26 @@ import styled from 'styled-components';
 
 
 export const StyledContainer = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Title = styled.div`
-margin-top: 4rem;
-font-size: 5rem;
-font-family: "Roboto";
-font-weight: 100;
-color: ${props => props.theme.palette.primary[300]};
-cursor: default;
+  margin-top: 4rem;
+  font-size: 5rem;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 100;
+  color: ${props => props.theme.palette.primary[300]};
+  cursor: default;
 `;
 
 export const Subtitle = styled.div`
-margin-top: 0rem;
-font-size: 1.6rem;
-font-family: "Roboto";
-font-weight: 100;
-color: ${props => props.theme.palette.primary[300]};
-cursor: default;
+  margin-top: 0rem;
+  font-size: 1.6rem;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 100;
+  color: ${props => props.theme.palette.primary[300]};
+  cursor: default;
 `;
